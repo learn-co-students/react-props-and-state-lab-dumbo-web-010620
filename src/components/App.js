66632e9e4594b3676daf1cf04d839e12300fe191
github username <github email address>
2020-03-26 09:16:45 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,47 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    this.setState({
+      filters: {
+        type: event.target.value
+      }
+    })
+  }
+
+  onFindPetsClick = (event) => {
+    // console.log("event.target:", event.target, 
+    // "you clicked the find pets button")
+    // fetch appropriate pets based on this.state.filters.type
+
+    let url = this.state.filters.type === 'all' ?
+    '/api/pets' : `/api/pets?type=${this.state.filters.type}`
+    
+    fetch(url).then(resp=>resp.json())
+    .then(jsonObj => this.setState({pets: jsonObj}))
+  }
+
+  onAdoptPet = (petId) => {
+    const pets = this.state.pets.map(pet => {
+      return pet.id === petId ? { ...pet, isAdopted: true } : pet 
+    })
+    this.setState({pets: pets})
+  }
+  
+  // confused about how to get an event out of this callback
+  // so I can reset the UI like I did in the obsolete version below
+
+  // onAdoptPet = (event, id) => {
+  //   event.target.className = event.target.className === "ui primary button" ? "ui disabled button" : "ui primary button"
+  //   console.log(event.target)
+  //   console.log(id)
+  //   // event.target.className === "ui primary button" ? event.target.className = "ui disabled button" : 
+  // }
+
+  // componentDidMount
+
   render() {
+    // console.log(this.state.pets)
     return (
       <div className="ui container">
         <header>
@@ -24,10 +64,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
